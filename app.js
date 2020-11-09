61bea48a7d6570e481242020c48110003be49c55
StoneShiShang <43474457+Stoneshishang@users.noreply.github.com>
2020-11-09 09:11:58 -0500

    sample axios.post added

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const axios = require('axios');
+const { localhost } = require('./config');
 
-
-const homeStartingContent = "Alsideeq bookstore is an online platform to sell available books provided by Abu Baker Alsideeq Isalmic Center (ABAIC) in Hamtramck, Michigan. ABAIC has a variety and large amounts of books in different Islamic topics and Arabic grammar in their bookstore."
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
 
 //create app using express
 const app = express();
@@ -15,7 +16,6 @@ app.use(bodyParser.urlencoded({extended: true}));
 //let express know that static files are held in the public folder
 app.use(express.static("public"));
 
-
 app.get("/", function(req, res){
   res.render("home");
 });
@@ -24,12 +24,58 @@ app.get("/signin", function(req, res){
   res.render("signin");
 });
 
+app.post("/signin", function(req, res){
+ let logOnUserName_    = req.body.userName;
+ let logOnPassword_ = req.body.logOnPassword;
+ const body={ 
+  username : logOnUserName_,
+  password : logOnPassword_
+};
+  axios.post( `${localhost}/Security/Login`,
+  body
+  )
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+});
+
 app.get("/register", function(req, res){
   res.render("register");
 });
 
+app.post("/register", function(req, res){
+  let registerFirstName_    =       req.body.registerFirstName;
+  let registerLastName_     =       req.body.registerLastName;
+  let registerEmailAddress_ =       req.body.registerEmailAddress;
+  let registerUsername_     =       req.body.registerUsername;
+  let registerPassword_     =       req.body.registerPassword;
+  let registerAddress1_     =       req.body.registerAddress1;
+  let registerAddress2_     =       req.body.registerAddress2;
+  let registerInputCity_    =       req.body.registerInputCity;
+  let registerInputState_   =       req.body.registerInputState;
+  let registerInputZip_     =       req.body.registerInputZip;
+});
+
 app.get("/customerservice", function(req, res){
   res.render("customerservice");
+});
+
+app.post("/customerservice", function(req, res){
+  let custServEmail_       = req.body.custServEmail;
+  let custServOrderNumber_ = req.body.custServOrderNumber;
+  let custServText_        = req.body.custServText;
+});
+
+
+app.get("/signedonalready", function(req, res){
+  res.render("signedonalready");
+});
+
+app.post("/signedonhome", function(req, res){
 });
 
 
