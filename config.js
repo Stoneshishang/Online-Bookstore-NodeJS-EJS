fcfe547ff1267b1ffd4f81274b19fd6e1fdc6b7e
StoneShiShang <43474457+Stoneshishang@users.noreply.github.com>
2020-12-05 23:41:44 -0500

    add both localhost URL and the live URL

--- config.js
+++ config.js
@@ -1,4 +1,9 @@
-const localhost="https://alsideeq-book-store-api.herokuapp.com"
+// const localhost="https://alsideeq-book-store-api.herokuapp.com"
+// module.exports = {
+//   localhost
+// }
+
+const localhost="http://localhost:5000"
 module.exports = {
   localhost
 }
