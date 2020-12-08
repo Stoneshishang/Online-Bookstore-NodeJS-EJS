21fc8c74877562997c83c7ae92a962485af04a05
StoneShiShang <43474457+Stoneshishang@users.noreply.github.com>
2020-12-08 10:29:45 -0500

    changed the url for deployment

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
-// const localhost="https://alsideeq-book-store-api.herokuapp.com"
-// module.exports = {
-//   localhost
-// }
-
-const localhost="http://localhost:5000"
+const localhost="https://alsideeq-book-store-api.herokuapp.com"
 module.exports = {
   localhost
 }
+
+// const localhost="http://localhost:5000"
+// module.exports = {
+//   localhost
+// }
